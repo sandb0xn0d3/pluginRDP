ea8e108b72cb918efb7cb5c3388beaa35a2bcc9b
sandb0xn0d3 <164109276+sandb0xn0d3@users.noreply.github.com>
2025-07-15 18:48:35 +0800

    Create install-plugin.sh

---
+++ install-plugin.sh
@@ -0,0 +1,12 @@
+install_chrome_extension () {
+  preferences_dir_path="/opt/google/chrome/extensions"
+  pref_file_path="$preferences_dir_path/$1.json"
+  upd_url="https://clients2.google.com/service/update2/crx"
+  mkdir -p "$preferences_dir_path"
+  echo "{" > "$pref_file_path"
+  echo "  \"external_update_url\": \"$upd_url\"" >> "$pref_file_path"
+  echo "}" >> "$pref_file_path"
+  echo Added \""$pref_file_path"\" ["$2"]
+}
+install_chrome_extension "gcbalfbdmfieckjlnblleoemohcganoc" "Ui-Vision"
+install_chrome_extension "bbdhfoclddncoaomddgkaaphcnddbpdh" "RekCapthca"
