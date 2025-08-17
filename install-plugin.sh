11237c4cd00059267a8d3c668f7a547067366b48
sandb0xn0d3 <164109276+sandb0xn0d3@users.noreply.github.com>
2025-08-18 04:29:20 +0800

    Update install-plugin.sh

--- install-plugin.sh
+++ install-plugin.sh
@@ -8,5 +8,18 @@ install_chrome_extension () {
   echo "}" >> "$pref_file_path"
   echo Added \""$pref_file_path"\" ["$2"]
 }
+
+install_brave_extension () {
+  preferences_dir_path="/opt/brave.com/brave/extensions"
+  pref_file_path="$preferences_dir_path/$1.json"
+  upd_url="https://clients2.google.com/service/update2/crx"
+  mkdir -p "$preferences_dir_path"
+  echo "{" > "$pref_file_path"
+  echo "  \"external_update_url\": \"$upd_url\"" >> "$pref_file_path"
+  echo "}" >> "$pref_file_path"
+  echo Added \""$pref_file_path"\" ["$2"]
+}
+
 install_chrome_extension "gcbalfbdmfieckjlnblleoemohcganoc" "Ui-Vision"
-install_chrome_extension "bbdhfoclddncoaomddgkaaphcnddbpdh" "RekCapthca"
+install_brave_extension "gcbalfbdmfieckjlnblleoemohcganoc" "Ui-Vision"
+# install_chrome_extension "bbdhfoclddncoaomddgkaaphcnddbpdh" "RekCapthca"
